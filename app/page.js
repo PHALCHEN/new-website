3c47e116b57d26689b402b1c3372772ab3e73514
Chen <phalchen.ip@gmail.com>
2023-12-17 14:59:06 +0700

    halo

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
+  return <div>hello halo</div>;
+};
 
-export default HomePage
+export default HomePage;
